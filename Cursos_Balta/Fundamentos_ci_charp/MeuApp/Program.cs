95ec7c46eb903842d8dfcc80f517dcdaab4624b4
Tarcio Passos Freitas <tarcio.freitas@toodoo.com.br>
2022-04-19 14:38:45 -0300

    aulas variáveis e constantes

--- Cursos_Balta/Fundamentos_ci_charp/MeuApp/Program.cs
+++ Cursos_Balta/Fundamentos_ci_charp/MeuApp/Program.cs
@@ -7,6 +7,20 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
+            //int idade; correto inicia com zero
+            //int idade = 25; correto inicia com 25
+            //var idade = 25; correto inicia com 25
+            //var idade; //errado
+
+            //const int IDADE_MINIMA; //correto inicia com zero
+            //const int IDADE_MINIMA = 25; //correto inicia com 25
+            //const var IDADE_MINIMA = 25; //errado
+            //const IDADE_MINIMA; // errado
+
+
+
+            var texto = "Testando";
+            Console.WriteLine(texto);
             Console.WriteLine("Hello world!");
         }
     }
