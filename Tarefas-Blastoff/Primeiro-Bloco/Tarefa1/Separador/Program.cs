f44f8f68d787f3e279d0306f14247e179dddb5de
Tarcio Passos Freitas <tarcio.freitas@toodoo.com.br>
2022-04-25 16:56:13 -0300

    Tarefa 1 - Blastoff - Separador de unidades

--- Tarefas-Blastoff/Primeiro-Bloco/Tarefa1/Separador/Program.cs
+++ Tarefas-Blastoff/Primeiro-Bloco/Tarefa1/Separador/Program.cs
@@ -6,28 +6,32 @@ namespace Separador
     {
         static void Main(string[] args)
         {
-            int number_digit;
-           
+            bool itsPossible;
+            var number_digit = 0;
+
             do
             {
                 Console.Clear();
-                number_digit = int.Parse(Console.ReadLine());
-                Separate(number_digit);
-            } while (number_digit < 0 || number_digit > 9999 || number_digit == null);
+                System.Console.WriteLine("Enter a number between 0 and 9999:");
+                itsPossible = int.TryParse(Console.ReadLine(), out number_digit);
+            } while (!itsPossible || number_digit < 0 || number_digit > 9999);
+
+            Separate(number_digit);
         }
 
         static void Separate(int value)
         {
+            Console.Clear();
             int u = value / 1 % 10;
             int d = value / 10 % 10;
             int c = value / 100 % 10;
             int uM = value / 1000 % 10;
 
             System.Console.WriteLine($@"
-                                        {u}
-                                        {d}
-                                        {c}
-                                        {uM}");
+            Unidade           =   {u}
+            Dezena            =   {d}
+            Centena           =   {c}
+            Unidade de milhar =   {uM}");
         }
     }
 }
