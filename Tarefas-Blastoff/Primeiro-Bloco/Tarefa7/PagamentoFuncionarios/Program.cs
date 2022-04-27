4fb571f5c78e3d17668b99e4bb42416290c7d386
Tarcio Passos Freitas <tarcio.freitas@toodoo.com.br>
2022-04-27 18:51:38 -0300

    Tarefa 7 - blastoff - Pagamento de funcionários ainda terá modificações

---
+++ Tarefas-Blastoff/Primeiro-Bloco/Tarefa7/PagamentoFuncionarios/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace PagamentoFuncionarios // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bem-vindo ao sistema de salário com comissões");
+
+
+
+            double[] array_salarios = new double[9] { 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+            double[] array_indices = new double[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            double indice;
+            double indice_maximo;
+
+            //EntradaDados(array_salarios);
+            System.Console.WriteLine("===================================================");
+            foreach (double salario in array_salarios)
+            {
+                indice = ((salario / 100) - 2);
+
+                indice_maximo = array_salarios.Length - 1;
+
+                Limitador(indice, indice_maximo);
+                array_indices[(int)indice] += 1;
+            }
+            System.Console.WriteLine("===============================================");
+
+            foreach (double testando in array_indices)
+            {
+
+                System.Console.WriteLine(testando);
+            }
+
+
+
+
+
+
+        }
+
+        static void Limitador(double indice, double indice_maximo)
+        {
+            if (indice > indice_maximo)
+            {
+                indice = indice_maximo;
+            }
+
+        }
+
+        static void EntradaDados(double[] array_salarios)
+        {
+            double y;
+            int contador = 9;
+            bool x_aux;
+            double x = 0;
+            System.Console.WriteLine("Quantidade de vendas brutas do Mês de 9 funcionários:");
+            for (int i = 0; i < contador; i++)
+            {
+                x_aux = double.TryParse(Console.ReadLine(), out x);
+                if (!x_aux && x < 0)
+                {
+                    EntradaDados(array_salarios);
+                }
+                else
+                {
+                    y = 200 + (0.09 * x);
+                    array_salarios[i] = y;
+                }
+            }
+
+        }
+    }
+}
