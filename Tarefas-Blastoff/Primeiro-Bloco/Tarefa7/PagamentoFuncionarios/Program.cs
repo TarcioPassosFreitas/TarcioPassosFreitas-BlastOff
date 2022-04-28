4ab8b7ca9cab8fee6e9d1e7d27b0c2dd59360bbb
Tarcio Passos Freitas <tarcio.freitas@toodoo.com.br>
2022-04-28 08:35:53 -0300

    Alterações no Pagamento funcionários

--- Tarefas-Blastoff/Primeiro-Bloco/Tarefa7/PagamentoFuncionarios/Program.cs
+++ Tarefas-Blastoff/Primeiro-Bloco/Tarefa7/PagamentoFuncionarios/Program.cs
@@ -9,24 +9,24 @@ namespace PagamentoFuncionarios
             System.Console.WriteLine("Bem-vindo ao sistema de salário com comissões");
 
 
-            double[] array_salarios = new double[9];
-            //double[] array_salarios = new double[9] { 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+            //double[] array_salarios = new double[9];
+            double[] array_salarios = new double[9] { 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
             double[] array_indices = new double[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            double indice;
-            double indice_maximo;
+            double indice_array;
+            double indice_maximo_array;
 
-            EntradaDados(array_salarios);
-            System.Console.WriteLine("===================================================");
+            //EntradaDados(array_salarios); //Problema na entrada de dados, mas questão não necessariamente pede entrada de dados
+            System.Console.WriteLine("==================================================="); //Regra de negócio
             foreach (double salario in array_salarios)
             {
-                indice = ((salario / 100) - 2);
+                indice_array = ((salario / 100) - 2);
 
-                indice_maximo = array_salarios.Length - 1;
+                indice_maximo_array = array_salarios.Length - 1;
 
-                Limitador(indice, indice_maximo);
-                array_indices[(int)indice] += 1;
+                Limitador(indice_array, indice_maximo_array);
+                array_indices[(int)indice_array] += 1;
             }
-            System.Console.WriteLine("===============================================");
+            System.Console.WriteLine("==============================================="); //Confirmar que os valores estão certos
 
             foreach (double testando in array_indices)
             {
@@ -41,7 +41,7 @@ namespace PagamentoFuncionarios
 
         }
 
-        static void Limitador(double indice, double indice_maximo)
+        static void Limitador(double indice, double indice_maximo) //Limita caso o valor passe do valor pedido
         {
             if (indice > indice_maximo)
             {
@@ -50,27 +50,27 @@ namespace PagamentoFuncionarios
 
         }
 
-        static void EntradaDados(double[] array_salarios)
-        {
-            double y;
-            int contador = 9;
-            bool x_aux;
-            double x = 0;
-            System.Console.WriteLine("Quantidade de vendas brutas do Mês de 9 funcionários:");
-            for (int i = 0; i < contador; i++)
-            {
-                x_aux = double.TryParse(Console.ReadLine(), out x);
-                if (!x_aux && x < 0)
-                {
-                    EntradaDados(array_salarios);
-                }
-                else
-                {
-                    y = 200 + (0.09 * x);
-                    array_salarios[i] = y;
-                }
-            }
+        // static void EntradaDados(double[] array_salarios)
+        // {
+        //     double y;
+        //     int contador = 9;
+        //     bool x_aux;
+        //     double x = 0;
+        //     System.Console.WriteLine("Quantidade de vendas brutas do Mês de 9 funcionários:");
+        //     for (int i = 0; i < contador; i++)
+        //     {
+        //         x_aux = double.TryParse(Console.ReadLine(), out x);
+        //         if (!x_aux && x < 0)
+        //         {
+        //             EntradaDados(array_salarios);
+        //         }
+        //         else
+        //         {
+        //             y = 200 + (0.09 * x);
+        //             array_salarios[i] = y;
+        //         }
+        //     }
 
-        }
+        // }
     }
 }
